d5813ecbe02c32b0f730ccada2b5d421b6aa57b4
Hakiki Tech <57021097+hakikicode@users.noreply.github.com>
2026-03-28 02:49:53 -0700

    Update contestants.js

--- api/contestants.js
+++ api/contestants.js
@@ -7,10 +7,6 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
   const response = await fetch(
     "https://api.github.com/repos/hakikicode/kwara-talents-harvest/contents/public/contestants"
   );
